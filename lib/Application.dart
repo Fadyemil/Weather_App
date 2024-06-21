469656a69110a3971920c84a49737b5632c1aa72
fady emil <fady46t45f@gmail.com>
2024-06-21 17:41:02 +0300

    add page for change page in App

---
+++ lib/Application.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/manger/Appcubit.dart';
+import 'package:weather_app/manger/Appstate.dart';
+import 'package:weather_app/widget/NavigationBar.dart';
+import 'package:weather_app/widget/getPage.dart';
+
+class ApplicationPage extends StatelessWidget {
+  const ApplicationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: BlocBuilder<SelectPageCubit, SelectPageState>(
+        builder: (context, state) {
+          return Scaffold(
+            body: getPage((state.index)),
+            bottomNavigationBar: Container(
+              width: 430,
+              height: 83,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: KNavigationBar(
+                selectedIndex: state.index,
+                onItemTapped: (int x) {
+                  BlocProvider.of<SelectPageCubit>(context).chage(index: x);
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
