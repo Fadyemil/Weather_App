6d988ee858a5445273968a3a50f2b6581d9aca90
fady emil <fady46t45f@gmail.com>
2024-06-24 01:29:10 +0300

    add WeaterSevice

---
+++ lib/Service/WeatherService.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:dio/dio.dart';
+import 'package:weather_app/models/Weather_Model.dart';
+
+class WeatherService {
+  final Dio dio;
+  WeatherService(this.dio);
+
+  final String baseUrl = 'https://api.weatherapi.com/v1';
+  final String ApiKey = '2c4a608571ac40e982a202508242306';
+
+  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio.get(
+          '$baseUrl/forecast.json?key=$ApiKey&q=$cityName&days=2&aqi=no&alerts=no');
+      print(response);
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errorMessage = e.response?.data['error']['message'] ??
+          'oops there was an error, please try again';
+      throw Exception(errorMessage);
+    } catch (e) {
+      throw Exception(
+          'oops there was an error, please try again .🤷‍♂️🤷‍♂️🤷‍♂️${e.toString()}');
+    }
+  }
+}
